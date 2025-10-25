b3f3f825ebda8aa8b41c7c4b311dbfeb79b03fc0
MeghanaDommaraju13 <meghanaraju13@gmail.com>
2025-10-25 11:27:46 +0530

    hi

---
+++ my-tailwind-project/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 justify-center">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      Text in Red
+    </div>
+  );
+}
+
+export default App;
